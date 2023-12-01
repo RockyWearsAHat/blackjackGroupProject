52392a5ee9a9e3777c693dab6eebe2ff9bc9e403
RockyWearsAHat <alexwaldmann2004@gmail.com>
2023-11-30 18:43:09 -0700

    More logic for winning and dealing, current issue with card hand calculation 2 aces + any other amount always defaults to one ace being 11, once ace being 1, and the other value, causing an auto bust when two aces should equal out to 2

--- game.js
+++ game.js
@@ -1,10 +1,15 @@
 //STORE A GLOBAL VARIABLE FOR THE DECKID SO A NEW DECK IS NOT GENERATED EVERY RELOAD
 const globalDeckId = sessionStorage.getItem("deckId");
 
+let gameOver = false;
+
 const users = {
   PLAYER: "player",
   DEALER: "dealer",
 };
+
+const userHand = document.getElementById("player1-cards-container");
+const dealerHand = document.getElementById("dealer-cards-container");
 
 const getNewDeck = async () => {
   if (globalDeckId) return globalDeckId;
@@ -46,6 +51,7 @@ const shuffleDeck = async (
 
     if (json.success) {
       // console.log(json);
+      return new Response(json);
     } else {
       return new Error("Error Shuffling Deck");
     }
@@ -72,6 +78,7 @@ const drawCard = async (
       // console.log(json);
       const cardCode = json.cards[0].code;
       await addCardToHand(deck, cardCode, handToAssign);
+      return new Response(json);
     } else {
       return new Error("Error Drawing Card");
     }
@@ -99,13 +106,14 @@ const addCardToHand = async (
     if (json.success) {
       // console.log(json);
       if (handToAssign == users.PLAYER) {
-        getUserHand(deck, handToAssign);
+        return new Response(await getUserHand(deck, handToAssign));
       } else {
         const li = document.createElement("li");
         li.id = `dealerCard${json.piles.dealer.remaining - 1}`;
-        li.classList = "card dealerCard";
+        li.classList = "card dealerCard hidden";
         li.innerHTML = `<img src="https://www.deckofcardsapi.com/static/img/back.png" class="cardBack"/><img src="" class="cardFront"/>`;
         dealerHand.appendChild(li);
+        return new Response(json.piles.dealer.remaining - 1);
       }
     } else {
       return new Error("Error Adding Card To Hand");
@@ -115,8 +123,6 @@ const addCardToHand = async (
   }
 };
 
-const userHand = document.getElementById("player1-cards-container");
-const dealerHand = document.getElementById("dealer-cards-container");
 const getUserHand = async (deck = "", user = users) => {
   if (!deck || !user) {
     return new Error("Error Getting Hand, DECK ID AND USER MUST BE DECLARED");
@@ -140,7 +146,10 @@ const getUserHand = async (deck = "", user = users) => {
         for (let i = 0; i < json.piles.dealer.cards.length; i++) {
           const currentCardWrapper = document.getElementById(`dealerCard${i}`);
           // console.log(json.piles.dealer.cards);
-          currentCardWrapper.children[1].src = json.piles.dealer.cards[i].image;
+          if (!currentCardWrapper.classList.contains("hidden")) {
+            currentCardWrapper.children[1].src =
+              json.piles.dealer.cards[i].image;
+          }
         }
       }
     } else {
@@ -159,9 +168,8 @@ const flipDealerHand = (deck, show = true) => {
   flipSelectedCards(dealerCards, show);
 };
 
-const flipSelectedCards = (card, show = true) => {
-  if (card.length > 1) {
-    console.log("running array");
+const flipSelectedCards = async (card, show = true) => {
+  if (Array.isArray(card) && card.length && card.length > 1) {
     card.forEach((card) => {
       gsap.set(card, {
         transformStyle: "preserve-3d",
@@ -182,8 +190,10 @@ const flipSelectedCards = (card, show = true) => {
         .to(card, { z: 0 }, 0.5);
 
       if (show) {
+        card.classList.remove("hidden");
         tl.play();
       } else {
+        card.classList.add("hidden");
         tl.reverse(0);
       }
     });
@@ -208,16 +218,116 @@ const flipSelectedCards = (card, show = true) => {
       .to(card, { z: 0 }, 0.5);
 
     if (show) {
-      console.log("showing");
+      card.classList.remove("hidden");
       tl.play();
     } else {
-      console.log("reversing");
+      card.classList.add("hidden");
       tl.reverse(0);
     }
   }
 };
 
-const getHand = async (deck = "") => {};
+const convertCardCodeToNumber = (cardCode, runningTotal = 0) => {
+  switch (cardCode) {
+    case "0":
+    case "JACK":
+    case "QUEEN":
+    case "KING":
+      return 10;
+    case "ACE":
+      return 1;
+    default:
+      return Number(cardCode);
+  }
+};
+
+const calcHandTotal = async (deck = "", user = users.PLAYER) => {
+  if (!deck || !user) {
+    return new Error("Error Getting Hand, DECK ID AND USER MUST BE DECLARED");
+  }
+  const res = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deck}/pile/${user}/list/`
+  );
+  if (res.ok) {
+    const json = await res.json();
+
+    let showingHandTotal = 0;
+    let handTotal = 0;
+
+    if (json.success) {
+      if (user === users.DEALER) {
+        for (let i = 0; i < json.piles.dealer.cards.length; i++) {
+          if (
+            document
+              .getElementById(`dealerCard${i}`)
+              .classList.contains("hidden")
+          ) {
+            handTotal += convertCardCodeToNumber(
+              json.piles.dealer.cards[i].value,
+              handTotal
+            );
+          } else {
+            showingHandTotal += convertCardCodeToNumber(
+              json.piles.dealer.cards[i].value,
+              showingHandTotal
+            );
+            handTotal += convertCardCodeToNumber(
+              json.piles.dealer.cards[i].value,
+              handTotal
+            );
+          }
+        }
+      } else {
+        for (let i = 0; i < json.piles.player.cards.length; i++) {
+          showingHandTotal += convertCardCodeToNumber(
+            json.piles.player.cards[i].value,
+            showingHandTotal
+          );
+          handTotal += convertCardCodeToNumber(
+            json.piles.player.cards[i].value,
+            handTotal
+          );
+        }
+      }
+
+      const elementToModify = document.getElementById(`${user}HandTotal`);
+      elementToModify.innerHTML = showingHandTotal;
+
+      if (user == users.DEALER) {
+        const dealerCardContainer = document.getElementById(
+          "dealer-cards-container"
+        );
+
+        let flag = false;
+        Object.values(dealerCardContainer.children).forEach((li) => {
+          if (li.classList.contains("hidden")) {
+            flag = true;
+          }
+        });
+
+        if (flag) elementToModify.innerHTML += ` ???`;
+      }
+
+      if (handTotal == 21) {
+        console.log(`${user} has won!`);
+        elementToModify.innerHTML = 21;
+        gameOver = true;
+        return Promise.resolve(handTotal);
+      } else if (handTotal > 21) {
+        console.log(`${user} has busted!`);
+        elementToModify.innerHTML = `${handTotal} / BUST`;
+        gameOver = true;
+        return Promise.resolve(100);
+      } else {
+        return Promise.resolve(showingHandTotal);
+      }
+    } else {
+      return new Error("Error Getting Hand");
+    }
+  } else {
+    return new Error("Error Getting Hand");
+  }
+};
 
 const startGame = async () => {
   const newDeck = await getNewDeck();
@@ -229,14 +339,54 @@ const startGame = async () => {
   await drawCard(newDeck, 1, users.PLAYER);
   await drawCard(newDeck, 1, users.DEALER);
 
-  setTimeout(() => {
-    flipDealerHand(newDeck, true);
+  const card0 = document.getElementById("dealerCard0");
+  await flipSelectedCards(card0);
+  await getUserHand(newDeck, users.DEALER);
 
-    setTimeout(() => {
-      const dealerCard2 = document.getElementById("dealerCard1");
-      flipSelectedCards(dealerCard2, false);
-    }, 3000);
-  }, 500);
+  await flipSelectedCards(gsap.utils.toArray(".playerCard"));
+
+  const dealerRes = await calcHandTotal(newDeck, users.DEALER);
+  const playerRes = await calcHandTotal(newDeck, users.PLAYER);
+
+  if (dealerRes === 21 || playerRes === 21) {
+    gameOver = true;
+    flipDealerHand(newDeck, true);
+    await calcHandTotal(newDeck, users.DEALER);
+  }
+
+  const hitBtn = document.getElementById("play-again-button");
+
+  const playerCardContainer = document.getElementById(
+    "player1-cards-container"
+  );
+
+  hitBtn.addEventListener("click", async () => {
+    if (!gameOver) {
+      await drawCard(newDeck, 1, users.PLAYER);
+
+      setTimeout(async () => {
+        await flipSelectedCards(
+          playerCardContainer.children[playerCardContainer.children.length - 1]
+        );
+
+        const res = await calcHandTotal(newDeck, users.PLAYER);
+
+        console.log(res);
+        if (res === 100) {
+          setTimeout(async () => {
+            flipDealerHand(newDeck, true);
+            await calcHandTotal(newDeck, users.DEALER);
+          }, 1000);
+        } else if (res === 21) {
+          flipDealerHand(newDeck, true);
+          await calcHandTotal(newDeck, users.DEALER);
+        }
+      }, 200);
+
+      // flipSelectedCards(card, true);
+      // console.log(lastIndex);
+    }
+  });
 };
 
 startGame();
